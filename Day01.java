66b37f9dc6ced32a30a3aac1b4080f1cdfce1ad2
Nurhidayanti022 <jialiaa2233@gmail.com>
2023-10-09 18:42:54 +0700

    first commit

---
+++ Day01.java
@@ -0,0 +1,15 @@
+
+package day01;
+
+public class Day01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("hallo dunia " );
+    }
+    
+}
