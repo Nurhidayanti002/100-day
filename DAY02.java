7dbe5cb64d160619ffb3913b89a3b6e07ffc454a
melyni05 <melyani633@gmail.com>
2023-10-10 09:07:25 +0700

    day2

---
+++ DAY02.java
@@ -0,0 +1,19 @@
+
+package day02;
+
+public class DAY02 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+         String nama= "Nurhidayanti";
+        String prodi = "informatika";
+        String Motifasi ="Tidak ada kata gagal dalam hidup,yang ada hanya sukses dan belum berhasil";
+        
+        System.out.println( " haiii.. SEMOGA HARI KAMU DAN AKU SELALU CERIA & BAHAGIA" );
+        System.out.println( "sebelumnya Perkenalkan saya ");
+        System.out.println( "Nama =" + nama );
+        System.out.println( "Prodi =" + prodi );
+        System.out.println( "Motifasi hari ini =" + Motifasi );
+    }
+    
+}
--- Day01.java
+++ Day01.java
@@ -8,8 +8,15 @@ public class Day01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String nama= "Nurhidayanti";
+        int Umur = 20;
+        String prodi = "informatika";
+        String pagi ="SEMANGAT";
         
-        System.out.println("hallo dunia " );
+        System.out.println( "nama =" + nama );
+        System.out.println( "umur =" + Umur );
+        System.out.println( " prodi =" + prodi );
+        System.out.println( "Motifasi hari ini =" + pagi );
     }
     
 }
