c897f5ef1f6b1e9b39cc3e51328fc74d19c62afb
melyni05 <melyani633@gmail.com>
2023-10-11 14:30:11 +0700

    day3

---
+++ Day3.java
@@ -0,0 +1,15 @@
+
+package day3;
+
+public class Day3 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String a = "bingung mau tulis apa";
+        int angka1 = 20;
+        int angka2 = 10;
+        System.out.println(a);
+        System.out.println (angka1 + angka2 );
+    }
+    
+}
