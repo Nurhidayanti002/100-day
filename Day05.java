26f3d02258471141ff51baaa3349dbcc5d51f5c8
melyni05 <melyani633@gmail.com>
2023-10-13 17:06:36 +0700

    day5

---
+++ Day05.java
@@ -0,0 +1,21 @@
+
+package pkg100day;
+import java.util.Scanner;
+
+public class Day05 {
+    public static void main (String [] args){
+        int angka= 0;
+        Scanner scan = new Scanner (System.in);
+        System.out.println("masukkan angka=");
+        angka = scan.nextInt();
+        if (angka % 2 == 0){
+           System.out.println ("," + "angka ini genap ");
+        }
+           else
+           System.out.println ("," + "angka ini ganjil");
+        }
+        }
+        
+        
+        
+    
