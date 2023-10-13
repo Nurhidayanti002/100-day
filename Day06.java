3700493ac95b9d6e13711a8e4a5964ba0f4afb0a
melyni05 <melyani633@gmail.com>
2023-10-14 06:16:13 +0700

    day6

---
+++ Day06.java
@@ -0,0 +1,11 @@
+
+package pkg100day;
+
+public class Day06 {
+    public static void main (String[]args){
+    String [] nama ={"tata","riska","kia","lala","lulu","momo"};
+        for (int a = 0; a < nama.length; a++) {
+        System.out.println("nama teman"+a+ "=" + nama [a]  );
+    }
+    }
+}
