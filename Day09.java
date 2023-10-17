23d60d37478d8f658eb444880325d0003cd4b3f2
melyni05 <melyani633@gmail.com>
2023-10-17 11:06:56 +0700

    day9

---
+++ Day09.java
@@ -0,0 +1,17 @@
+
+package day09;
+
+public class Day09 {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    
+    for (int i = 1; i<= 15 ; i++){
+        System.out.println(i+" hallo selamat pagi dan semangat ngoding");
+        
+    }
+    }
+    
+}
