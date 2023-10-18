c0de797d160bc0079210fb6d5d8e713d49d929b2
melyni05 <melyani633@gmail.com>
2023-10-18 19:00:07 +0700

    day10

---
+++ DAY10.java
@@ -0,0 +1,18 @@
+
+package pkg100day;
+
+import java.util.Scanner;
+
+public class DAY10 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String kabar,bahagia,bersyukur;
+       Scanner sc = new Scanner (System.in);
+        System.out.println("bagaimana kabar hari ini:");
+        kabar = sc.nextLine();
+        System.out.println("adakah sesuatu yang membuatmu bahagia hari ini:");
+        bahagia = sc.nextLine();
+        System.out.println("sudah kah anda bersyukur hari ini:");
+        bersyukur = sc.nextLine();
+    }
+}
