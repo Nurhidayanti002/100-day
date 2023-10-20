28489b00434b8e1ca22a9dd7fdd0789e1a94d6dd
melyni05 <melyani633@gmail.com>
2023-10-21 00:39:10 +0700

    Day13

---
+++ Day13.java
@@ -0,0 +1,11 @@
+
+package pkg100day;
+
+
+public class Day13 {
+    public static void main (String [] args){
+        for (int a = 1; a <= 30; a += 3){
+            System.out.println ( "MENCETAK BILANGAN GENAP: " + a);
+        }
+    }
+}
