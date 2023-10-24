fa17659413b07624ca04ff588b717a4171734f6a
melyni05 <melyani633@gmail.com>
2023-10-24 13:34:14 +0700

    day16

---
+++ Day16.java
@@ -0,0 +1,20 @@
+
+package pkg100day;
+import java.util.Scanner;
+public class Day16 {
+    public static void main (String [] args){
+        // membuat kalkulator sesat
+    Scanner sc = new Scanner(System.in);
+    System.out.print("kalkulator PENJUMLAHAN sesat");
+    System.out.println();
+    System.out.print ("masukkan angka pertama :");
+    int angka = sc.nextInt();
+    System.out.print("masukkan angka ke dua :" );
+    int angka2 = sc.nextInt();
+    System.out.print("jumlah = " +  angka / angka2);
+    System.out.println();
+    
+   
+    
+}
+}
