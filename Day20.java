bc542b2304ece4cf9ad2033690c3ff438d65ed0e
melyni05 <melyani633@gmail.com>
2023-10-28 19:49:54 +0700

    for semangat

---
+++ Day20.java
@@ -0,0 +1,15 @@
+
+package pkg100day;
+
+public class Day20 {
+    public static void main(String[] args) {
+     
+        for (int i = 0; i <= 20 ; i++){
+        System.out.println ( "semangat" );
+
+        }
+        
+        
+    
+    }
+}
