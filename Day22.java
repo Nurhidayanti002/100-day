7eee0b9fc39a3da5b4722640e98e248c4dbe333e
melyni05 <melyani633@gmail.com>
2023-10-30 19:49:17 +0700

    Hallo

---
+++ Day22.java
@@ -0,0 +1,11 @@
+
+package pkg100day;
+import java.util.Scanner;
+public class Day22 {
+    public static void main (String [] args){
+    Scanner sc = new Scanner (System.in);
+    System.out.print("Masukkan Nama : ");
+    String Nama = sc.nextLine();
+    System.out.println("Hallo, " + Nama + " Bagaimana kabar anda hari ini, Saya harap anda bahagia hari ini");
+    }
+}
