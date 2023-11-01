3d4a21462ec2e0cc1910764bf03d5c3d35d71ce7
melyni05 <melyani633@gmail.com>
2023-11-01 21:08:19 +0700

    for

---
+++ Day24.java
@@ -0,0 +1,14 @@
+
+package pkg100day;
+
+public class Day24 {
+    public static void main(String[] args) {
+        // membuat program menampilkan bilangan genap 1-50 dan habis di bagi 4
+     for (int i = 7; i <= 77; i ++ ){
+         if ((i % 2 )== 0){
+             System.out.println(i);
+         }
+     }
+         }
+    }
+
