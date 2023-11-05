1f2fd61bae73f748529db9e5487391d012f7c9db
melyni05 <melyani633@gmail.com>
2023-11-05 16:17:49 +0700

    while

---
+++ Day28.java
@@ -0,0 +1,12 @@
+
+package pkg100day;
+public class Day28 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10 ){
+                System.out.println("Selamat sore " +i + "-kali \n ");
+                i++ ;
+        }
+        
+    }
+}
