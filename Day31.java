15194dc57c98671d5a8bbeaff9e92c34717a8b2d
melyni05 <melyani633@gmail.com>
2023-11-08 12:25:25 +0700

    kalimat

--- Day31.java
+++ Day31.java
@@ -1,13 +1,16 @@
 
 package pkg100day;
 import java.util.Arrays;
+import java.util.Scanner;
 public class Day31 {
     public static void main(String[] args) {
-        String  Kalimat = "Di mana kaki berpijak di situ langit di junjung";
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Masukkan kalimat anda : ");
+        String Kalimat = sc.nextLine();
         String [] ArraySplit = Kalimat.split(("\\s"));
         System.out.println(Arrays.toString(ArraySplit));
-        for (String s :ArraySplit ){
-            System.out.println(s);
+        for (String Hasil :ArraySplit ){
+            System.out.println(Hasil);
         }
     }
 }
