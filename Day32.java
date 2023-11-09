323dd138c2b51abd24396f2f12dc41fe549685bf
melyni05 <melyani633@gmail.com>
2023-11-09 19:58:31 +0700

    mengabungan kata

---
+++ Day32.java
@@ -0,0 +1,11 @@
+
+package pkg100day;
+
+public class Day32 {
+    public static void main(String[] args) {
+          String Kata1 = "Pergi ";
+        String Kata2 = "Kuliah";
+        String Kata3 = Kata1.concat(Kata2);
+        System.out.println(Kata3 );
+    }
+}
