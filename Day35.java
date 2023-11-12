a5f4ab3c2f07780476c1ae61b67a50123285979a
melyni05 <melyani633@gmail.com>
2023-11-12 22:22:16 +0700

    abjad

---
+++ Day35.java
@@ -0,0 +1,12 @@
+
+package pkg100day;
+
+public class Day35 {
+    public static void main(String[] args) {
+        char c;
+
+    for(c = 'A'; c <= 'Z'; c++){
+      System.out.print(c + " ");
+    }
+    }
+}
