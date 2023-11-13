73a98f3af6cff9130cf6936f505ac84ceb9d384d
melyni05 <melyani633@gmail.com>
2023-11-13 21:41:25 +0700

    perkalian

---
+++ Day36.java
@@ -0,0 +1,13 @@
+
+package pkg100day;
+
+public class Day36 {
+    public static void main(String[] args) {
+        int num = 4;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+ 
+}
