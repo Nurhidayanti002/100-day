ab903ce79fbb710a1041c7e10471c65efab9e17b
melyni05 <melyani633@gmail.com>
2023-11-14 20:23:55 +0700

    Day 37

---
+++ Day37.java
@@ -0,0 +1,31 @@
+package pkg100day;
+
+public class Day37 {
+    public static void main(String[] args) {
+        
+       // Tipe data primitif untuk angka bulat
+        int Angka = 10;
+        long bigNumber = 1000000000L;
+
+        // Tipe data primitif untuk angka desimal
+        double DesimalNomor1 = 3.14;
+        float DesimalNumber2 = 2.5f;
+
+        // Tipe data primitif untuk karakter
+        char Char = 'A';
+
+        // Tipe data primitif untuk boolean
+        boolean Java = true;
+
+        // Menampilkan nilai variabel ke konsol
+        System.out.println("Nomor: " + Angka);
+        System.out.println("Big Nomor: " + bigNumber);
+        System.out.println("Desimal Nomor (1): " + DesimalNomor1);
+        System.out.println("Desimal Nomor (2): " + DesimalNumber2);
+        System.out.println("Tipe data Char: " + Char);
+        System.out.println("Is Java Fun? " + Java);
+    
+    }
+}
+    
+
