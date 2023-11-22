96d3e61edd61a099bf286037b0c15f0dd69c16ff
melyni05 <melyani633@gmail.com>
2023-11-22 22:40:04 +0700

    java

--- Day45.java
+++ Day45.java
@@ -4,13 +4,12 @@ import java.util.Scanner;
 public class Day45 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.println("Masukkan angka n :");
+        System.out.print("Masukkan angka n :");
         int Angka =sc.nextInt();
-         int Bilangangenap = 0;
+         System.out.println("bilangan genap dari angka 1 sampai "+ Angka + " adalah ");
         for (int i = 1 ; i <= Angka; i++){
-            System.out.println(i);
             if (i % 2 == 0){
-         System.out.println("Angka genapnya :" + i);
+         System.out.print(  i + " ");
             }
         }
         
