94a79d8a207eacdff7a29febb5a15cb1a8216382
melyni05 <melyani633@gmail.com>
2023-11-27 22:21:19 +0700

    JAVA

---
+++ Day50.java
@@ -0,0 +1,31 @@
+
+package pkg100day;
+import java.util.Scanner;
+public class Day50 {
+    public static void main(String[] args) {
+    String[] buah = new String[5];
+
+        // membuat scanner
+        Scanner Sc = new Scanner(System.in);
+
+        // mengisi data ke array
+        for( int i = 0; i < buah.length; i++ ){
+            System.out.print("Buah ke-" + i + ": ");
+            buah[i] = Sc.nextLine();
+        }
+
+        System.out.println("---------------------------");
+
+        // menampilkan semua isi array
+        for( String b : buah ){
+            System.out.println(b);
+        }
+
+    }
+}
+
+
+
+    
+
+    
